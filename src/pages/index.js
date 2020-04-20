88920f03a5ea2e3511e48e748cec98b5eaf35b97
julia-francais <ju.francais@gmail.com>
2020-04-20 10:59:05 +0200

    change font-family

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,17 @@ import React, { Component } from "react"
 import { Link } from "gatsby"
 
 import { Cards, Chart, CountryPicker } from "../components"
-import styles from "./Index.css"
+import styles from "../styles/Index.css"
 import { fetchData } from "../api"
 
-// import Layout from "../components/layout"
+import { ThemeProvider } from "@material-ui/styles"
+
 import Header from "../components/header"
 import coronaImage from "../images/image.png"
 
-// import Image from "../components/image"
 import SEO from "../components/seo"
 
+import theme from "../styles/theme"
 class IndexPage extends Component {
   state = {
     data: {},
@@ -33,20 +34,20 @@ class IndexPage extends Component {
   render() {
     const { data, country } = this.state
     return (
-      // <Layout>
-      <div className="container">
-        <SEO title="Home" />
-        <img
-          className={styles.img}
-          src={coronaImage}
-          alt="COVID-19"
-          style={{ marginTop: "50px" }}
-        />
-        <Cards data={data} />
-        <CountryPicker handleCountryChange={this.handleCountryChange} />
-        <Chart data={data} country={country} />
-      </div>
-      // </Layout>
+      <ThemeProvider theme={theme}>
+        <div className="container">
+          <SEO title="Home" />
+          <img
+            className={styles.img}
+            src={coronaImage}
+            alt="COVID-19"
+            style={{ marginTop: "50px" }}
+          />
+          <Cards data={data} />
+          <CountryPicker handleCountryChange={this.handleCountryChange} />
+          <Chart data={data} country={country} />
+        </div>
+      </ThemeProvider>
     )
   }
 }
---
+++ src/styles/theme.js
@@ -0,0 +1,9 @@
+import { createMuiTheme } from "@material-ui/core/styles"
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: ["Inter", "sans-serif"].join(","),
+  },
+})
+
+export default theme

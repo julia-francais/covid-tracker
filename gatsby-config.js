6e4c984117d2708a36b084168bb12f79cefed81f
julia-francais <ju.francais@gmail.com>
2020-04-21 10:25:56 +0200

    add config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Covid-19 Tracker`,
+    description: `Covid19-tracker to follow day by day evolution of the disease`,
+    author: `juliafrancais`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
